e570f118e46fe480738a795b607117a54895ef0e
Freddy Järvå <freddy.a.jarva@gmail.com>
2021-02-27 21:07:52 +0100

    remove hacky double border and use box-shadow instead

--- src/ActiveItemPane.svelte
+++ src/ActiveItemPane.svelte
@@ -1,34 +1,35 @@
 <script>
-import ItemBox from "./boxes/ItemBox.svelte";
-import MedallionBox from "./boxes/MedallionBox.svelte";
+  import ItemBox from "./boxes/ItemBox.svelte";
+  import MedallionBox from "./boxes/MedallionBox.svelte";
 
   export let items;
 
-  console.log(items)
+  console.log(items);
 </script>
 
 <div class="pane">
   {#each items as item}
-    {#if item.type === 'medallion'}
-      <MedallionBox {...item}/>
+    {#if item.type === "medallion"}
+      <MedallionBox {...item} />
     {:else}
-      <ItemBox {...item}/>
+      <ItemBox {...item} />
     {/if}
   {/each}
 </div>
 
 <style type="text/scss">
-  @import 'src/theme.scss';
+  @import "src/theme.scss";
   .pane {
     grid-row: 1 / span 10;
     grid-column: 1 / span 11;
     display: grid;
     grid-template-columns: repeat(10, 34px) 10px;
     grid-template-rows: repeat(10, 34px) 10px;
-    border: $zelda-green solid 3px;
+    border: white solid 3px;
     padding-left: 10px;
     padding-top: 10px;
     margin-bottom: -20px;
     border-radius: 2%;
+    box-shadow: 0 0 0 3px $zelda-green;
   }
 </style>
--- src/Timer.svelte
+++ src/Timer.svelte
@@ -1,42 +1,39 @@
 <script>
-  import { formatTime } from './utils'
+  import { formatTime } from "./utils";
 
   let startTime;
   let elapsedTime = 0;
   let interval;
-  $: isRunning = interval ? true : false
-  let isFinished = false
+  $: isRunning = interval ? true : false;
+  let isFinished = false;
 
   function startTimer() {
-    startTime = Date.now()
+    startTime = Date.now();
     interval = setInterval(() => {
-      elapsedTime = Date.now() - startTime
-    }, 100)
+      elapsedTime = Date.now() - startTime;
+    }, 100);
   }
 
   function stopTimer() {
-    clearInterval(interval)
-    interval = null
+    clearInterval(interval);
+    interval = null;
   }
 
   function reset() {
-    clearInterval(interval)
-    interval = null
-    isFinished = false
-    elapsedTime = 0
+    clearInterval(interval);
+    interval = null;
+    isFinished = false;
+    elapsedTime = 0;
   }
 
   function finish() {
-    isFinished = true
-    stopTimer()
+    isFinished = true;
+    stopTimer();
   }
-
 </script>
 
-
-
-<div class='Timer'>
-  <div class={`time ${isFinished ? 'finished' : null}`}>
+<div class="Timer">
+  <div class={`time ${isFinished ? "finished" : null}`}>
     {formatTime(elapsedTime)}
   </div>
   {#if !isRunning && !isFinished}
@@ -44,12 +41,12 @@
   {:else}
     <button on:click={reset}>RESET</button>
   {/if}
-  <button class='button' on:click={finish} disabled={!isRunning}>FINISH</button>
+  <button class="button" on:click={finish} disabled={!isRunning}>FINISH</button>
 </div>
 
 <style lang="scss">
-  @import 'src/theme.scss';
-  
+  @import "src/theme.scss";
+
   .Timer {
     border-top: whitesmoke;
 
@@ -58,41 +55,51 @@
     display: grid;
     grid-template-columns: 1fr 1fr;
     grid-template-rows: 2fr 1fr;
-    border: $zelda-yellow solid 3px;
+    box-shadow: 0 0 0 3px $zelda-yellow;
+    border: whitesmoke solid 3px;
     border-radius: 5px;
 
     button {
-        margin: 4px;
-        color: whitesmoke;
-        background-color: black;
-        border-color: $zelda-yellow;
-        border-bottom: 1px solid hsl(43, 74%, 49%);
-        border-right: 1px solid goldenrod;
-        font-family: 'Press Start 2P';
-        font-size: 12px;
-        margin: -4px 8px 10px 10px;
+      margin: 4px;
+      color: whitesmoke;
+      background-color: black;
+      border-color: $zelda-yellow;
+      border-bottom: 1px solid hsl(43, 74%, 49%);
+      border-right: 1px solid goldenrod;
+      font-family: "Press Start 2P";
+      font-size: 12px;
+      margin: -4px 8px 10px 10px;
     }
 
     button:focus {
-        outline: none;
+      outline: none;
     }
 
     button:disabled {
-        color: grey;
-        border-color: grey;
+      color: grey;
+      border-color: grey;
+    }
+
+    button:focus {
+      outline: none;
+    }
+
+    button:disabled {
+      color: grey;
+      border-color: grey;
     }
 
     .time {
-        font-size: 38px;
-        color: rgb(255, 255, 255);
-        align-self: center;
-        justify-self: center;
-        padding: 10px;
-        grid-column: span 2;
+      font-size: 38px;
+      color: rgb(255, 255, 255);
+      align-self: center;
+      justify-self: center;
+      padding: 10px;
+      grid-column: span 2;
     }
 
     .time.finished {
-        color: $zelda-green;
+      color: $zelda-green;
     }
   }
 </style>
