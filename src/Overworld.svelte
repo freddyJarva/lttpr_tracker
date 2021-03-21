6de5a68db20daca0e00a2009133390dc237f8191
Freddy Järvå <freddy.a.jarva@gmail.com>
2021-03-21 23:55:11 +0100

    WIP - complete mess

--- src/Overworld.svelte
+++ src/Overworld.svelte
@@ -1,17 +1,62 @@
 <script>
-  const overworldLight = "images/overworld-light.png";
-  const overworldDark = "images/overworld-dark.png";
+  import { originalSize } from "./mapcontent";
+  export let image;
+  export let world;
+  export let entrances;
+
+  let renderedSize;
+
+  $: console.log("Rendered size: ", renderedSize);
+
+  function positionInGrid(axisValue) {
+    let ratio = originalSize / renderedSize;
+    let position = axisValue / ratio / 4;
+    console.log("Position: ", position);
+    return Math.floor(position);
+  }
 </script>
 
-<div class="MapBox" style="background-image: url({overworldLight})" />
+<div
+  class="MapBox"
+  style="background-image: url({image})"
+  bind:clientWidth={renderedSize}
+>
+  <!-- <div class="MapBox"> -->
+  {#if renderedSize !== undefined}
+    {#each entrances as entrance}
+      <span
+        style="grid-column-start: {positionInGrid(
+          entrance.x
+        )}; grid-row-start: {positionInGrid(
+          entrance.y
+        )}; background-image: url(icons/door-36x36.svg)"
+      />
+      <!-- <span /> -->
+      <!-- <div style="background-image: url(icons/door-36x36.svg)" /> -->
+    {/each}
+  {/if}
+</div>
 
 <style type="text/scss">
+  @import "src/theme.scss";
   .MapBox {
-    //  outline: rgb(40, 38, 53) solid 1px;
-    //  justify-self: center;
     background-size: contain;
-    //  resize: ;
-    grid-row-end: 2;
-    grid-column-end: 2;
+    grid-row-end: span 1;
+    grid-column-end: span 1;
+
+    display: grid;
+    grid-template-columns: repeat($map-cell-total, $map-cell-size);
+    grid-template-rows: repeat($map-cell-total, $map-cell-size);
+  }
+
+  span {
+    // background: 1em 1em no-repeat;
+    background-size: cover;
+    outline: rgb(209, 7, 220) solid 1px;
+  }
+
+  .door {
+    fill: yellow;
+    opacity: 1;
   }
 </style>
