f0babbac55ad851ae37d884998f506e93db3b1b9
Freddy Järvå <freddy.a.jarva@gmail.com>
2021-03-26 20:27:09 +0100

    add custom door icon to map

--- src/mapcontent.js
+++ src/mapcontent.js
@@ -1,4 +1,19 @@
+import * as L from "leaflet";
+
 const overworldDarkLight = "images/overworld-dark-light.png";
+
+export const doorIcon = L.icon({
+  iconUrl: "icons/map-marker-door.svg",
+  shadowUrl: "icons/map-marker-shadow.svg",
+
+  iconSize: [30, 50], // size of the icon
+  shadowSize: [30, 50], // size of the shadow
+  iconAnchor: [15, 50], // point of the icon which will correspond to marker's location
+  popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+  shadowAnchor: [2, 36], // the same for the shadow
+});
+
+export const defaultIcon = new L.Icon.Default();
 
 export const mapUnit = 16;
 export const imageHeight = 4096;
