39d08c7e01520478510fae4a2c26cc3d0e859399
Freddy Järvå <freddy.a.jarva@gmail.com>
2021-04-02 15:41:23 +0200

    remove unused icons

--- src/map/icons.ts
+++ src/map/icons.ts
@@ -33,17 +33,3 @@ export const mapIcons = {
     shadowAnchor: [1, 22], // the same for the shadow
   }),
 };
-
-export const doorIcon = L.icon({
-  iconUrl: "icons/map-marker-door-green.svg",
-  shadowUrl: "icons/map-marker-shadow.svg",
-
-  iconSize: [20, 30], // size of the icon
-  shadowSize: [20, 30], // size of the shadow
-  iconAnchor: [10, 30], // point of the icon which will correspond to marker's location
-  popupAnchor: [0, -32], // point from which the popup should open relative to the iconAnchor
-  tooltipAnchor: [12, -20],
-  shadowAnchor: [1, 22], // the same for the shadow
-});
-
-export const defaultIcon = new L.Icon.Default();
