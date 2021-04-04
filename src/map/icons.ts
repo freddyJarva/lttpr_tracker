44e705f30772e52028331fd4fa0a39f93a77ee1d
Freddy Järvå <freddy.a.jarva@gmail.com>
2021-04-05 00:30:27 +0200

    change entrance icon to higher contrasting one

--- src/map/icons.ts
+++ src/map/icons.ts
@@ -3,16 +3,24 @@ import { InteractiveMarker, isInteractiveMarker, MarkerData } from "./markers";
 
 const mapIcons = {
   entrance: L.icon({
-    iconUrl: "icons/map-marker-door-green.svg",
-    shadowUrl: "icons/map-marker-shadow.svg",
-
-    iconSize: [20, 30], // size of the icon
-    shadowSize: [20, 30], // size of the shadow
-    iconAnchor: [10, 30], // point of the icon which will correspond to marker's location
-    popupAnchor: [0, -32], // point from which the popup should open relative to the iconAnchor
-    tooltipAnchor: [12, -20],
-    shadowAnchor: [1, 22], // the same for the shadow
+    iconUrl: "icons/map-marker-entrance.svg",
+    iconSize: [12, 26], // size of the icon
+    iconAnchor: [6, 26], // point of the icon which will correspond to marker's location
+    popupAnchor: [0, -26], // point from which the popup should open relative to the iconAnchor
+    tooltipAnchor: [12, -13],
   }),
+
+  // entrance: L.icon({
+  //   iconUrl: "icons/map-marker-door-green.svg",
+  //   shadowUrl: "icons/map-marker-shadow.svg",
+
+  //   iconSize: [20, 30], // size of the icon
+  //   shadowSize: [20, 30], // size of the shadow
+  //   iconAnchor: [10, 30], // point of the icon which will correspond to marker's location
+  //   popupAnchor: [0, -32], // point from which the popup should open relative to the iconAnchor
+  //   tooltipAnchor: [12, -20],
+  //   shadowAnchor: [1, 22], // the same for the shadow
+  // }),
   glitch: L.icon({
     iconUrl: "icons/map-marker-glitch-improved.svg",
 
