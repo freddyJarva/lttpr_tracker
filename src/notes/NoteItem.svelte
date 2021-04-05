67cb1ce2681f9d3b172a5b75b3eaff3b1a707cdb
Freddy Järvå <freddy.a.jarva@gmail.com>
2021-04-05 23:10:30 +0200

    add note pane skeleton

---
+++ src/notes/NoteItem.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let text = "HELLO WORLD";
+</script>
+
+<span contenteditable="true">{text}</span>
+
+<style lang="scss">
+  span {
+    font-size: 14px;
+    border: 1px solid red;
+    padding: 4px;
+    margin: 4px;
+  }
+</style>
