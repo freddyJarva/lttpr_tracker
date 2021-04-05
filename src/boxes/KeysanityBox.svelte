ee658b72302fdac7069218dca05e2a66198b3890
Freddy Järvå <freddy.a.jarva@gmail.com>
2021-04-05 21:32:10 +0200

    move item tracker assets to new folder

--- src/boxes/KeysanityBox.svelte
+++ src/boxes/KeysanityBox.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { bigKeyImage } from "../items";
   export let name;
   export let images = [];
   export let smallKeyMax = 0;
@@ -6,72 +7,75 @@
   export let smallKeyGoMode = 0;
   export let bigKey = false;
 
-  let keyCount = 0
+  let keyCount = 0;
   let activeRewardIndex = 0;
-  let bigKeyImage = 'images/big-key.png'
   let isDone = false;
   let hasBigKey = false;
   let dungeonState;
   let keyColor;
-  $: activeClass = isDone ? 'active' : 'inactive'
+  $: activeClass = isDone ? "active" : "inactive";
   $: if (keyCount < smallKeyGoMode) {
-    keyColor = 'red'
-    dungeonState = 'not clearable'
+    keyColor = "red";
+    dungeonState = "not clearable";
   } else if (keyCount < smallKeyMin) {
-    keyColor = 'gold'
-    dungeonState = 'Go Mode'
+    keyColor = "gold";
+    dungeonState = "Go Mode";
   } else if (keyCount < smallKeyMax && smallKeyMax - smallKeyMin) {
-    keyColor = 'blue'
-    dungeonState = 'full clear w. optimal key usage and items'
+    keyColor = "blue";
+    dungeonState = "full clear w. optimal key usage and items";
   } else {
-    keyColor = 'green'
-    dungeonState = 'all keys found'
+    keyColor = "green";
+    dungeonState = "all keys found";
   }
 
   function toggleDone() {
-    isDone = !isDone
+    isDone = !isDone;
   }
 
   function changeReward() {
-    activeRewardIndex = (activeRewardIndex + 1) % images.length
+    activeRewardIndex = (activeRewardIndex + 1) % images.length;
   }
 
   function incrementKeys() {
-    keyCount = (keyCount + 1) % (smallKeyMax + 1)
+    keyCount = (keyCount + 1) % (smallKeyMax + 1);
   }
 
   function toggleBigKey() {
-    hasBigKey = !hasBigKey
+    hasBigKey = !hasBigKey;
   }
-
 </script>
 
-<div class='DungeonBox' on:contextmenu|preventDefault={changeReward}>
-  <span class='keysanity-fragment {`dungeon-name-${activeClass}`}'>{name}</span>
+<div class="DungeonBox" on:contextmenu|preventDefault={changeReward}>
+  <span class="keysanity-fragment {`dungeon-name-${activeClass}`}">{name}</span>
   {#if images[activeRewardIndex]}
-    <img 
-      on:click|preventDefault={toggleDone} 
-      src={images[activeRewardIndex]} 
-      alt={name} 
-      class="keysanity-fragment {activeClass}"/>
+    <img
+      on:click|preventDefault={toggleDone}
+      src={images[activeRewardIndex]}
+      alt={name}
+      class="keysanity-fragment {activeClass}"
+    />
   {:else}
-    <span 
-      on:click|preventDefault={toggleDone} 
-      class='keysanity-fragment {activeClass} dungeon-reward'><strong>?</strong></span>
+    <span
+      on:click|preventDefault={toggleDone}
+      class="keysanity-fragment {activeClass} dungeon-reward"
+      ><strong>?</strong></span
+    >
   {/if}
-  <span 
-    class='keysanity-fragment {keyColor} key-count' 
+  <span
+    class="keysanity-fragment {keyColor} key-count"
     on:click|preventDefault={incrementKeys}
-    title={dungeonState}>
-      {keyCount}
+    title={dungeonState}
+  >
+    {keyCount}
   </span>
   {#if bigKey}
     <img
       on:click={toggleBigKey}
-      src={bigKeyImage} alt='Big Key' 
-      class="keysanity-fragment big-key" 
+      src={bigKeyImage}
+      alt="Big Key"
+      class="keysanity-fragment big-key"
       class:big-key-inactive={!hasBigKey}
-      />
+    />
   {/if}
 </div>
 
@@ -79,76 +83,75 @@
   @import "src/theme.scss";
 
   .DungeonBox {
-      color: whitesmoke;
-      justify-self: center;
+    color: whitesmoke;
+    justify-self: center;
+    grid-row-end: span 1;
+    grid-column: 11 / span 4;
+    width: 110%;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    font-family: Zelda;
+    font-size: 22px;
+
+    .green {
+      color: $zelda-green;
+      @include transition-mixin(color);
+    }
+
+    .blue {
+      color: $zelda-blue;
+      @include transition-mixin(color);
+    }
+
+    .red {
+      color: $zelda-red;
+      @include transition-mixin(color);
+    }
+
+    .gold {
+      color: $zelda-yellow;
+      @include transition-mixin(color);
+    }
+
+    .dungeon-reward {
+      margin-left: 6px;
+    }
+
+    .dungeon-name-active {
+      color: $zelda-green;
+    }
+
+    .keysanity-fragment {
       grid-row-end: span 1;
-      grid-column: 11 / span 4;
-      width: 110%;
-      display: grid;
-      grid-template-columns: 1fr 1fr 1fr 1fr;
-      font-family: Zelda;
-      font-size: 22px;
+      grid-column-start: span 1;
+      justify-self: center;
+    }
 
-      .green {
-        color: $zelda-green;
-        @include transition-mixin(color);
-      }
+    .key-count {
+      font-size: 30px;
+      margin-top: 0px;
+    }
 
-      .blue {
-        color: $zelda-blue;
-        @include transition-mixin(color);
-      }
+    .big-key {
+      width: 90%;
+      @include transition-mixin(all, 0.1s, ease-out);
+    }
 
-      .red {
-        color: $zelda-red;
-        @include transition-mixin(color);
-      }
+    .big-key-inactive {
+      filter: grayscale(1);
+      opacity: 0.3;
+    }
 
-      .gold {
-        color: $zelda-yellow;
-        @include transition-mixin(color);
-      }
+    span {
+      margin: 4px;
+    }
+    img {
+      width: 30px;
+    }
 
-      .dungeon-reward {
-        margin-left: 6px;
-      }
-
-      .dungeon-name-active {
-        color: $zelda-green;
-      }
-
-      .keysanity-fragment {
-        grid-row-end: span 1;
-        grid-column-start: span 1;
-        justify-self: center;
-      }
-
-      .key-count {
-        font-size: 30px;
-        margin-top: 0px;
-      }
-
-      .big-key {
-        width: 90%;
-        @include transition-mixin(all, .1s, ease-out)
-      }
-
-      .big-key-inactive {
-        filter: grayscale(1);
-        opacity: .3;
-      }
-
-      span {
-          margin: 4px;
-      }
-      img {
-          width: 30px;
-      }
-
-      .inactive {
-        filter: grayscale(0.4);
-        opacity: 0.6;
-      }
-
+    .inactive {
+      filter: grayscale(0.4);
+      opacity: 0.6;
+    }
   }
 </style>
