64b5f90601501b4d22be743937c4cb8c2404ca4e
Freddy Järvå <freddy.a.jarva@gmail.com>
2021-04-10 15:44:21 +0200

    move line creation into line.ts

--- src/map/line.ts
+++ src/map/line.ts
@@ -1,3 +1,4 @@
+import * as L from "leaflet";
 let currentColorIdx = 0;
 let colors = ["#F00", "#0F0", "#00F", "#FF0", "#F0F", "#0FF"];
 
@@ -5,4 +6,18 @@ export function lineColor(): string {
   let chosen = colors[currentColorIdx];
   currentColorIdx = (currentColorIdx + 1) % colors.length;
   return chosen;
+}
+
+export function createLine(point1: L.LatLng, point2: L.LatLng): L.Polyline {
+  return L.polyline([point1, point2], {
+    color: lineColor(),
+    opacity: 0.8,
+    weight: 6,
+  });
+}
+
+export function lineBetween(m1: L.Marker | L.LatLng, m2: L.Marker | L.LatLng) {
+  let point1 = m1 instanceof L.Marker ? m1.getLatLng() : m1;
+  let point2 = m2 instanceof L.Marker ? m2.getLatLng() : m2;
+  return createLine(point1, point2);
 }
