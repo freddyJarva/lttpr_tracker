03d44057744095b85fa4fbd7c07155b9aefcbdb2
Freddy Järvå <freddy.a.jarva@gmail.com>
2021-04-19 22:24:33 +0200

    add possibility for standalone notes

--- src/notes/NotePane.svelte
+++ src/notes/NotePane.svelte
@@ -1,14 +1,34 @@
 <script lang="ts">
+  import { selectTextOnFocus, blurOnEscape } from "./inputDirectives";
   import NoteItem from "./NoteItem.svelte";
-  import type { MapObject } from "../map/store";
+  import { MapObject } from "../map/store";
+  import { tick } from "svelte";
 
   export let notes: Array<MapObject>;
+
+  let newNoteText: string = "new note..";
+
+  function addNote(e: KeyboardEvent) {
+    if (e.key === "Enter") {
+      let note = new MapObject(null, null, newNoteText);
+      notes = [...notes, note];
+      newNoteText = "";
+    }
+  }
 </script>
 
 <div class="pane">
-  {#each notes as note (note.entranceId)}
+  {#each notes as note}
     <NoteItem {...note} />
   {/each}
+  <input
+    class="note-input"
+    use:selectTextOnFocus
+    use:blurOnEscape
+    type="text"
+    bind:value={newNoteText}
+    on:keydown={addNote}
+  />
 </div>
 
 <style type="text/scss">
@@ -17,5 +37,20 @@
     display: flex;
     flex-flow: row wrap;
     gap: 4px 4px;
+    margin-bottom: 8px;
+  }
+
+  .note-input {
+    max-width: 140px;
+    font-size: 14px;
+
+    text-align: center;
+
+    padding: 4px 8px 4px 10px;
+    margin-bottom: 8px;
+
+    border-radius: 6%;
+    font-size: 14px;
+    font-family: "Press Start 2P";
   }
 </style>
