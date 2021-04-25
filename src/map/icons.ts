1691b743e2493e09f4b32eabb2ded2950c60fe06
Freddy Järvå <freddy.a.jarva@gmail.com>
2021-04-25 11:39:49 +0200

    update map icons

--- src/map/icons.ts
+++ src/map/icons.ts
@@ -26,13 +26,11 @@ export interface IconData extends L.IconOptions {
 }
 
 const HYRULE_CASTLE = {
-  iconSize: [24, 26],
+  iconSize: [22, 26],
 };
 
 const DESERT_PALACE = {
-  iconSize: [28, 41],
-  iconAnchor: [10, 38],
-  popupAnchor: [0, -38],
+  iconSize: [30, 30],
 };
 
 const TURTLE_ROCK = {
@@ -57,24 +55,24 @@ export const iconDataList: Array<IconData> = [
   iconData("icons/map-marker-hyrulecastle-m.webp", HYRULE_CASTLE),
   iconData("icons/map-marker-hyrulecastle-w.webp", HYRULE_CASTLE),
   iconData("icons/map-marker-hyrulecastle-e.webp", HYRULE_CASTLE),
-  iconData("icons/map-marker-easternpalace.webp", { iconSize: [32, 32] }),
+  iconData("icons/map-marker-easternpalace.webp", { iconSize: [24, 24] }),
   iconData("icons/map-marker-desertpalace-m.webp", DESERT_PALACE),
   iconData("icons/map-marker-desertpalace-e.webp", DESERT_PALACE),
   iconData("icons/map-marker-desertpalace-w.webp", DESERT_PALACE),
   iconData("icons/map-marker-desertpalace-b.webp", DESERT_PALACE),
-  iconData("icons/map-marker-castletower.webp", { iconSize: [30, 32] }),
-  iconData("icons/map-marker-towerofhera.webp", { iconSize: [34, 30] }),
-  iconData("icons/map-marker-palaceofdarkness.webp", { iconSize: [32, 32] }),
-  iconData("icons/map-marker-swamppalace.webp", { iconSize: [30, 29] }),
-  iconData("icons/map-marker-skullwoods.webp", { iconSize: [30, 32] }),
-  iconData("icons/map-marker-thievestown.webp", { iconSize: [26, 19] }),
-  iconData("icons/map-marker-icepalace.webp", { iconSize: [32, 32] }),
-  iconData("icons/map-marker-miserymire.webp", { iconSize: [32, 34] }),
-  iconData("icons/map-marker-turtlerock-m.webp", TURTLE_ROCK),
-  iconData("icons/map-marker-turtlerock-w.webp", TURTLE_ROCK),
-  iconData("icons/map-marker-turtlerock-e.webp", TURTLE_ROCK),
-  iconData("icons/map-marker-turtlerock-b.webp", TURTLE_ROCK),
-  iconData("icons/map-marker-ganonstower.webp", { iconSize: [30, 32] }),
+  iconData("icons/map-marker-castletower.webp", { iconSize: [26, 26] }),
+  iconData("icons/map-marker-towerofhera.webp", { iconSize: [26, 26] }),
+  iconData("icons/map-marker-palaceofdarkness.webp", { iconSize: [30, 30] }),
+  iconData("icons/map-marker-swamppalace.webp", { iconSize: [30, 30] }),
+  iconData("icons/map-marker-skullwoods.webp", { iconSize: [30, 30] }),
+  iconData("icons/map-marker-thievestown.webp", { iconSize: [30, 30] }),
+  iconData("icons/map-marker-icepalace.webp", { iconSize: [30, 30] }),
+  iconData("icons/map-marker-miserymire.webp", { iconSize: [30, 30] }),
+  iconData("icons/map-marker-turtlerock-m.svg", TURTLE_ROCK),
+  iconData("icons/map-marker-turtlerock-w.svg", TURTLE_ROCK),
+  iconData("icons/map-marker-turtlerock-e.svg", TURTLE_ROCK),
+  iconData("icons/map-marker-turtlerock-b.svg", TURTLE_ROCK),
+  iconData("icons/map-marker-ganonstower.webp", { iconSize: [25, 25] }),
   iconData("icons/map-marker-ganon.webp", { iconSize: [39, 27] }),
   // Special locationa
   iconData("images/npc/sahasrahla.webp", { iconSize: [16, 24] }),
